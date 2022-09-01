50849c30ca3bacbfa71b05caee5828bcc2ced1a1
zherenh <zherenh@student.unimelb.edu.au>
2022-09-01 17:24:29 +1000

    debug linklist function

--- Practise/Day06/Insert.js
+++ Practise/Day06/Insert.js
@@ -22,16 +22,31 @@ insert a new node inside a linklist
     node5.next = new Node(6);
     var input = node1;
     function insert(head, index) {
+        // init the new node
+        var newNode = new Node(0);
+        // when index = 0; means insert the node at the beginning
+        if (index === 0) {
+            newNode.next = head;
+            return newNode;
+        }
+        // because the operation of linklist hae to operate the head.next and head.next.next.
+        // so index - 1 first, then looping
+        index = index - 1;
         var output = head;
+        // go to the where should insert the node
         while (index > 0) {
+            if (head.next.next === undefined) {
+                head.next.next = newNode;
+                return output;
+            }
             head = head.next;
             index--;
         }
-        var tmp = head;
-        var newNode = new Node(0);
+        // replace
+        var tmp = head.next.next;
         head.next = newNode;
-        newNode.next = tmp.next.next;
+        newNode.next = tmp;
         return output;
     }
-    console.log(insert(input, 1));
+    console.log(insert(input, 6));
 })();
--- Practise/Day06/Insert.ts
+++ Practise/Day06/Insert.ts
@@ -31,17 +31,33 @@ insert a new node inside a linklist
         let input = node1;
 
         function insert(head: linklist, index:number): linklist {
+            // init the new node
+            let newNode:linklist = new Node(0);
+            // when index = 0; means insert the node at the beginning
+            if(index === 0){
+                newNode.next = head;
+                return newNode;
+            }
+            // because the operation of linklist hae to operate the head.next and head.next.next.
+            // so index - 1 first, then looping
+            index = index - 1;
             let output:linklist = head;
+            // go to the where should insert the node
             while(index > 0){
+                // add new node at the end
+                if(head.next.next === undefined){
+                    head.next.next = newNode;
+                    return output;
+                }
                 head = head.next;
                 index --;
             }
-            let tmp:linklist = head;
-            let newNode:linklist = new Node(0);
+            // replace
+            let tmp:linklist = head.next.next;
             head.next = newNode;
-            newNode.next = tmp.next.next;
+            newNode.next = tmp;
             return output;
         }
-        console.log(insert(input, 1));
+        console.log(insert(input, 6));
     }
 )();
