fe3671fc57f986bb5f00f9c36c84ea44580ae808
zherenh <zherenh@student.unimelb.edu.au>
2022-09-23 23:57:57 +1000

    mongoose

---
+++ 017mongodb/db/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post("/user/add", (req,res)=>{
+  console.log(req.body)
+
+  
+  res.send({
+    ok:1
+  })
+})
+
+module.exports = router;
