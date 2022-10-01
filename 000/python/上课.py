3f25f47535b8cdcc475448bca463d8e678a4fd28
zherenh <zherenh@student.unimelb.edu.au>
2022-10-01 15:30:50 +1000

    py

---
+++ 000/python/上课.py
@@ -0,0 +1,25 @@
+# print("Hello World")
+
+a = 1.34
+# print(a+a+a); print(a-a)
+
+print(f'hello{a:0.2f}world')
+# print("helloe world" + str(a) + "hhh")
+
+print( round(1/7, 3) )
+print(round(1/3, 4))
+
+# 小数点后4位
+print(5e-4)
+
+print( type(-6) )
+print( type(10.3) )
+print( type(99.0) )
+print( type(-1.0) )
+a = 4
+b = 2
+print( (a/b) )
+print( (5//2) )
+
+print((12100 - 4750)/50)
+
