7358c6854d55179a1502976cab5da883ac8e0e92
zherenh <zherenh@student.unimelb.edu.au>
2022-10-04 01:26:56 +1100

    aws and git vedio

--- 000/python/上课.py
+++ 000/python/上课.py
@@ -1,25 +1,44 @@
-# print("Hello World")
+from math import sqrt
+# import pandas as pd
+# # print("Hello World")
 
-a = 1.34
-# print(a+a+a); print(a-a)
+# a = 1.34
+# # print(a+a+a); print(a-a)
 
-print(f'hello{a:0.2f}world')
-# print("helloe world" + str(a) + "hhh")
+# print(f'hello{a:0.2f}world')
+# # print("helloe world" + str(a) + "hhh")
 
-print( round(1/7, 3) )
-print(round(1/3, 4))
+# print( round(1/7, 3) )
+# print(round(1/3, 4))
 
-# 小数点后4位
-print(5e-4)
+# # 小数点后4位
+# print(5e-4)
 
-print( type(-6) )
-print( type(10.3) )
-print( type(99.0) )
-print( type(-1.0) )
-a = 4
-b = 2
-print( (a/b) )
-print( (5//2) )
+# print( type(-6) )
+# print( type(10.3) )
+# print( type(99.0) )
+# print( type(-1.0) )
+# a = 4
+# b = 2
+# print( (a/b) )
+# print( (5//2) )
 
-print((12100 - 4750)/50)
+a = 3/100
+print(a)
+print(f"helle {a:.1%} world")
 
+b = 4
+print(sqrt(4))
+
+
+# fib = [1,2,3]
+# for x in fib:
+#     print(x)
+# print('hello')
+
+# y = 100
+# z = 101
+# if 99 < y and z < 101:
+#     print (y)
+
+print(type('a'))
