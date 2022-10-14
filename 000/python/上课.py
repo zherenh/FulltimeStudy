1c5aa343fd2787399507bfa9a0df07c0f657cf07
zherenh <zherenh@student.unimelb.edu.au>
2022-10-15 02:27:23 +1100

    class study

--- 000/python/上课.py
+++ 000/python/上课.py
@@ -1,44 +1,15 @@
-from math import sqrt
-# import pandas as pd
-# # print("Hello World")
+import string
 
-# a = 1.34
-# # print(a+a+a); print(a-a)
-
-# print(f'hello{a:0.2f}world')
-# # print("helloe world" + str(a) + "hhh")
-
-# print( round(1/7, 3) )
-# print(round(1/3, 4))
-
-# # 小数点后4位
-# print(5e-4)
-
-# print( type(-6) )
-# print( type(10.3) )
-# print( type(99.0) )
-# print( type(-1.0) )
-# a = 4
-# b = 2
-# print( (a/b) )
-# print( (5//2) )
-
-a = 3/100
-print(a)
-print(f"helle {a:.1%} world")
-
-b = 4
-print(sqrt(4))
+from traitlets import Int
 
 
-# fib = [1,2,3]
-# for x in fib:
-#     print(x)
-# print('hello')
+def demo(name, age = 20)->int:
+    print(name)
+    print(age)
+    return age
 
-# y = 100
-# z = 101
-# if 99 < y and z < 101:
-#     print (y)
+demo("Ann", 10)
 
-print(type('a'))
+def __init__(self):
+    self.A = 100
+    
